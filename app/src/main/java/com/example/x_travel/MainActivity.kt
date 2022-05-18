b5ff4728dd0b605d1c90eeddf99290b26b244aed
NongDamGom1216 <hyun5735@naver.com>
2022-05-18 17:50:16 +0900

    220518 홈메뉴 레이아웃

--- app/src/main/java/com/example/x_travel/MainActivity.kt
+++ app/src/main/java/com/example/x_travel/MainActivity.kt
@@ -1,36 +1,93 @@
 package com.example.x_travel
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.navigation.ui.AppBarConfiguration
 import com.amazonaws.mobile.client.AWSMobileClient
 import com.amazonaws.mobile.client.Callback
 import com.amazonaws.mobile.client.UserStateDetails
+import com.example.x_travel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var selectedIndex: Int = 0;
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val signOut_button = findViewById<Button>(R.id.signOut_button) // 로그아웃 버튼
 
-        // 로그아웃 버튼
-        signOut_button.setOnClickListener {
-            AWSMobileClient.getInstance().initialize(
-                applicationContext,
-                object : Callback<UserStateDetails?> {
-                    override fun onResult(userStateDetails: UserStateDetails?) {
-                        // 로그아웃 후 로그인 창으로 이동
-                        AWSMobileClient.getInstance().signOut()
-                        val i = Intent(this@MainActivity, AuthActivity::class.java)
-                        startActivity(i)
-                        finish()
-                    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-                    override fun onError(e: Exception) {}
-                })
+        val motionLayout = findViewById<MotionLayout>(R.id.motion_container)
+
+        val v1 = findViewById<View>(R.id.v1)
+        val v2 = findViewById<View>(R.id.v2)
+        val v3 = findViewById<View>(R.id.v3)
+
+        v1.setOnClickListener {
+            if (selectedIndex == 0) return@setOnClickListener
+
+            motionLayout.setTransition(R.id.s2, R.id.s1) //orange to blue transition
+            motionLayout.transitionToEnd()
+            selectedIndex = 0;
         }
+        v2.setOnClickListener {
+            if (selectedIndex == 1) return@setOnClickListener
+
+            if (selectedIndex == 2) {
+                motionLayout.setTransition(R.id.s3, R.id.s2)  //red to orange transition
+            } else {
+                motionLayout.setTransition(R.id.s1, R.id.s2) //blue to orange transition
+            }
+            motionLayout.transitionToEnd()
+            selectedIndex = 1;
+        }
+        v3.setOnClickListener {
+            if (selectedIndex == 2) return@setOnClickListener
+
+            motionLayout.setTransition(R.id.s2, R.id.s3) //orange to red transition
+            motionLayout.transitionToEnd()
+            selectedIndex = 2;
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_tab, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.signOut_button -> {
+                AWSMobileClient.getInstance().initialize(
+                    applicationContext,
+                    object : Callback<UserStateDetails?> {
+                        override fun onResult(userStateDetails: UserStateDetails?) {
+                            // 로그아웃 후 로그인 창으로 이동
+                            AWSMobileClient.getInstance().signOut()
+                            val i = Intent(this@MainActivity, AuthActivity::class.java)
+                            startActivity(i)
+                            finish()
+                        }
+
+                        override fun onError(e: Exception) {}
+                    })
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     // 뒤로가기 2번 눌러야 종료
